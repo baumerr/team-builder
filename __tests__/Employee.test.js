c026395baf5eaf20c2ca254ce2a12b80108ed78c
randy <baumer.randy@gmail.com>
2021-10-31 14:39:48 -0400

    writing base tests for employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,7 +2,25 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Randy');
+    data = {
+        name: 'Randy',
+        id: 1,
+        email: 'randy.baumer@hotmail.com'
+    }
+    const employee = new Employee(data);
 
     expect(employee.name).toBe('Randy');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test('gets employees name', () => {
+    data = {
+        name: 'Randy',
+        id: 1,
+        email: 'randy.baumer@hotmail.com'
+    }
+    const employee = new Employee(data);
+
+    expect(employee.getName()).toBe(employee.name);
 })
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
+
+inquirer
+  .prompt([
+      {
+          type: 'input',
+          name: 'name',
+          message: 'What is your name?'
+      }
+  ]).then((answers) => {
+      const employee = new Employee(answers);
+      console.log('Hello ' + employee.name + '!');
+  })
